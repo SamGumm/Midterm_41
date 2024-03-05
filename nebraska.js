edefbac3228c10ef3fbcc79d80bc3eb380c61aff
braed <hegartyb@iastate.edu>
2024-03-04 18:34:11 -0600

    Adding WI IL and NE bird pages

---
+++ nebraska.js
@@ -0,0 +1,27 @@
+fetch("./data.json").then(response => response.json())
+.then(birds => testingBirds(birds));
+
+function testingBirds(birdsData){
+    var cards = document.getElementsByClassName("card shadow-sm");
+    var body = document.getElementsByClassName("card-body");
+    let cardCounter = 0;
+    for(var i = 0; i < birdsData.birds.length; i++){
+        if(birdsData.birds[i].state == "Nebraska"){
+            let img = document.createElement("img");
+            img.src = birdsData.birds[i].image
+            img.style.width = "100%";
+            img.style.height = "auto";
+
+            paragraph = document.createElement("p");
+            paragraph.classList.add("card-text");
+            paragraph.innerText = birdsData.birds[i].description;
+
+            cards[cardCounter % cards.length].appendChild(img);
+            body[cardCounter % cards.length].appendChild(paragraph);
+            cardCounter++;
+        }
+    }
+}
+
+
+
